c9ac3929768c867af11a404d4218183142782f11
osparking <jbpark03@gmail.com>
2024-02-15 13:38:49 +0900

    마일 적립 및 점수 계산 메소드 구현

--- src/main/java/space/bum/junit/mileage/Mileage.java
+++ src/main/java/space/bum/junit/mileage/Mileage.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import lombok.Getter;
 import space.bum.junit.airport.Passenger;
 
-@Getter
 public class Mileage {
 
   public static final int VIP_FACTOR = 10;
@@ -15,9 +14,17 @@ public class Mileage {
   private Map<Passenger, Integer> mileages = new HashMap<>();
   private Map<Passenger, Integer> points = new HashMap<>();
 
+  public Map<Passenger, Integer> getPoints() {
+    return points;
+  }
+
   public void addMileage(Passenger passenger, int miles) {
+    mileages.put(passenger, mileages.getOrDefault(passenger, 0) + miles);
   }
 
   public void calculateGivenPoints() {
+    mileages.keySet().forEach(
+        passenger -> points.put(passenger, mileages.get(passenger)
+            / (passenger.isVip() ? VIP_FACTOR : REGULAR_FACTOR)));
   }
 }
